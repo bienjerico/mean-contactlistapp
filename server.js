e42bc22393ac2aeb00c5266d2df5b7bcc1fda571
Bien Jerico Cueto <bien.jerico@gmail.com>
2016-02-29 16:20:40 +0800

    mongojs to mongoose

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('contactlist',['contactlist']);
+// var db = mongojs('contactlist',['contactlist']);
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost:27017/contactlist');
+var ContactListSchema = new mongoose.Schema({
+  name : String,
+  email : String,
+  number : String
+});
+
+var Contacts = mongoose.model('Contacts',ContactListSchema);
+
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
 app.get('/contactlist',function(req,res){
 
-  db.contactlist.find(function(err,docs){
-    console.log(docs);
-    res.json(docs);
+  Contacts.find(function(err,docs){
+    if(err)
+      console.log(err);
+    else
+      console.log(docs);
+      res.json(docs);
   });
 
 });
@@ -19,9 +33,16 @@ app.get('/contactlist',function(req,res){
 app.post('/contactlist',function(req,res){
 
   console.log(req.body);
-  db.contactlist.insert(req.body,function(err,docs){
-    res.json(docs);
+  console.log("beforesave");
+  var contacts = new Contacts(req.body);
+
+  contacts.save(function(err,docs){
+    if(err)
+      console.log(err);
+    else
+      res.json(docs);
   });
+
 
 });
 
@@ -29,9 +50,14 @@ app.delete('/contactlist/:id',function(req,res){
 
   var id = req.params.id;
   console.log(id);
-  db.contactlist.remove({_id: mongojs.ObjectId(id)},function(err,doc){
+
+  Contacts.findByIdAndRemove(id, function (err, doc) {
+    if (err)
+      console.log(err);
+    else
       res.json(doc);
   });
+
 
 });
 
@@ -39,7 +65,10 @@ app.get('/contactlist/:id',function(req,res){
 
   var id = req.params.id;
   console.log(id);
-  db.contactlist.findOne({_id:mongojs.ObjectId(id)},function(err,doc){
+  Contacts.findById(id,function(err,doc){
+    if (err)
+      console.log(err);
+    else
       res.json(doc);
   });
 
@@ -49,11 +78,11 @@ app.get('/contactlist/:id',function(req,res){
 app.put('/contactlist/:id',function(req,res){
   var id = req.params.id;
   console.log(req.body.name);
-    db.contactlist.findAndModify({
-        query: {_id: mongojs.ObjectId(id)},
-        update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
-        new: true }, function(err,doc){
-            res.json(doc);
+    Contacts.findByIdAndUpdate(id, req.body , function(err,doc){
+      if (err)
+        console.log(err);
+      else
+          res.json(doc);
     });
 });
 
